2c6a252a0b5e3a907d85056122326ab526e0541c
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-17 14:54:46 +0700

    implementasi metode

--- Konstruktor.cpp
+++ Konstruktor.cpp
@@ -31,3 +31,17 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
     nim=iNim;
     nama=iNama;
 }
+
+void mahasiswa:: cetak(){
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
