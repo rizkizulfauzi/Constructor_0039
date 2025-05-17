0340c361d6e497f91bdab3dbf533f265d19a74eb
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-17 15:06:12 +0700

    menambahkan deklarasi

---
+++ mbrStatikfunc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    static int nim;
+public:
+int id;
+string nama;
+
+void setID();
+void printALL();
+
+static void setNim(int pNim){nim=pNim; /*definisi function*/}
+static int getNim(){return nim; /*Definisi Function*/}
+
+mahasiswa(string pnama):nama(pnama){setID();}
+};
