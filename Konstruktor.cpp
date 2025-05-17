f0962272a3558ef9bb67e4ff70878921eb8dcaf9
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-17 14:48:19 +0700

    menambahkan konstruktor mahasiswa(string iNama)

--- Konstruktor.cpp
+++ Konstruktor.cpp
@@ -22,3 +22,12 @@ mahasiswa :: mahasiswa(){
 mahasiswa :: mahasiswa(int iNim){
     nim=iNim;
 }
+
+mahasiswa:: mahasiswa(string iNama){
+    nama=iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim=iNim;
+    nama=iNama;
+}
