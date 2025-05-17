385c7bfeb9d0d6d2d7029f67f5c79293cc8883ca
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-17 14:57:25 +0700

    Tunjukkan penggunaan semua konstruktor mahasiswa dan tampilkan keluaran

--- Konstruktor.cpp
+++ Konstruktor.cpp
@@ -2,37 +2,51 @@
 #include <string>
 using namespace std;
 
-class mahasiswa{
-    private:
+class mahasiswa
+{
+private:
     int nim;
     string nama;
-    public:
+
+public:
     mahasiswa();
     mahasiswa(int);
     mahasiswa(string);
-    mahasiswa(int iNim,string iNama);
-void cetak();
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
 
-mahasiswa :: mahasiswa(){
-    nim =0;
-    nama= "";
+mahasiswa ::mahasiswa()
+{
+    nim = 0;
+    nama = "";
 }
 
-mahasiswa :: mahasiswa(int iNim){
-    nim=iNim;
+mahasiswa ::mahasiswa(int iNim)
+{
+    nim = iNim;
 }
 
-mahasiswa:: mahasiswa(string iNama){
-    nama=iNama;
+mahasiswa::mahasiswa(string iNama)
+{
+    nama = iNama;
 }
 
-mahasiswa :: mahasiswa(int iNim, string iNama){
-    nim=iNim;
-    nama=iNama;
+mahasiswa ::mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
 }
 
-void mahasiswa:: cetak(){
+void mahasiswa::cetak()
+{
+    cout << endl
+         << "Nim =" << nim << endl;
+    cout << "Nama =" << nama << endl;
+}
+
+int main()
+{
     mahasiswa mhs1;
     mahasiswa mhs2(20);
     mahasiswa mhs3("indra");
